12325c6c40f66b2a924393d338d462a6598d0c74
freetime <43937423+BeanGTA@users.noreply.github.com>
2022-08-05 16:40:34 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 source utils.sh
 
-YT_PATCHER_ARGS="-e microg-support -e hide-autoplay-button -e amoled -e custom-branding -e swipe-controls"
+YT_PATCHER_ARGS="-e microg-support -e hide-autoplay-button -e amoled -e custom-branding -e swipe-controls -e always-autorepeat"
 MUSIC_PATCHER_ARGS="-e microg-support"
 
 BUILD_YT=false
